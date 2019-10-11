b21db45edbffe18aa960e6721308ce729f6e34a9
gcmartinelli <gcmartinelli@gmail.com>
2019-10-11 02:55:31 +0000

    simplifying H()

--- entropy.py
+++ entropy.py
@@ -9,7 +9,7 @@ def H(data):
     ''' Calculate the entropy of a given data block '''
     entropy = 0
     for x in range(256):
-        p_x = data.count((x).to_bytes(1, 'little'))/len(data)
+        p_x = data.count(x)/len(data)
         if p_x > 0:
             entropy += -p_x*math.log(p_x, 2)
     return entropy
